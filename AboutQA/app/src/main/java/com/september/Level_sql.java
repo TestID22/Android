3faafbf9e2ca2cdd112d25154766d2de0edc2420
septemberEatMe <vitaliiseptember@gmail.com>
2019-12-27 21:10:26 +0200

    del mar

---
+++ AboutQA/app/src/main/java/com/september/Level_sql.java
@@ -0,0 +1,43 @@
+package com.september;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Level_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.level_1);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        TextView back = (TextView)findViewById(R.id.button_back_from_level_1);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Level_1.this, Levels.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+    @Override
+    public void onBackPressed(){
+        try{
+            Intent intent = new Intent(Level_1.this, Levels.class);
+            startActivity(intent);
+            finish();
+        }catch (Exception e){
+
+        }
+    }
+}
