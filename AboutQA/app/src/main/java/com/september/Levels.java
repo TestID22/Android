1c3764c4514e4cb32ce7cac033a2a0c414200988
septemberEatMe <vitaliiseptember@gmail.com>
2019-12-27 12:24:03 +0200

    create levelsMenu

--- AboutQA/app/src/main/java/com/september/Levels.java
+++ AboutQA/app/src/main/java/com/september/Levels.java
@@ -1,8 +1,12 @@
 package com.september;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,7 +17,49 @@ public class Levels extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.levels);
 
+        //Убрать шапку приложения
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button buttonback = (Button)findViewById(R.id.buttonback);
+        buttonback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    Intent intent = new Intent(Levels.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
+
+        //Кнопка выбора первого уровня - начало
+        TextView level1 = (TextView)findViewById(R.id.textViewBasics);
+        level1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(Levels.this, Level_1.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
+        //Кнопка выбора перовго уровня - конец
+    }
+    //Системная кнопка назад
+    @Override
+    public void onBackPressed(){
+        try{
+            Intent intent = new Intent(Levels.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }catch (Exception e){
+
+        }
     }
 }
